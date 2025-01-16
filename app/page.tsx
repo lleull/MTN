c18a4d18a2b848f4b8076a759e534aad3cb37264
Leul Mekonnen <leulmekonnen049@gmail.com>
2025-01-16 03:02:39 -0800

    Frontend finished

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <div className="w-full  flex flex-col items-center  justify-center bg-bgcustom">
       {/* Custom <Classifiation></Classifiation> */}
       <div className="flex relative w-full h-[140vh] overflow-hidden">
-        <Image src={HG} alt="sd" className="w-full absolute top-0 h-[70vh]" />
+        <Image src={HG} alt="sd" className="w-full absolute top-0 h-[20vh]" />
         <Image src={overLayer} alt="sd" className="w-full absolute top-0 h-[70vh]" />
 
         <Image src={Mountains} alt="sd" className="mountain" />
@@ -33,14 +33,14 @@ export default function Home() {
             <Image src={img2} color="white" alt="s" className="w-5 h-5" />
           </div>
 
-          <div className="w-1/2 ml-40 flex flex-col">
-            <h2 className="flex flex-row items-center gap-2  text-orangeLine">
+          <div className="w-1/2 ml-56 flex flex-col">
+            <h2 className="flex flex-row items-center text-2xl gap-2  text-orangeLine">
               <div className="w-[10vh] h-[2px] z-90 bg-orangeLine"></div>A Hiking guide
             </h2>
             <h1 className="text-[51px] w-2/3  font-chronic ">
               Be prepared for the Mountains and beyond!
             </h1>
-            <h3 className="flex flex-row items-center gap-4 mt-20">
+            <h3 className="flex flex-row items-center gap-4 mt-10">
               Scroll down <Image src={arrow} alt="sd" width={10} height={10} />{' '}
             </h3>
           </div>
@@ -56,3 +56,6 @@ export default function Home() {
     </div>
   )
 }
+
+
+
